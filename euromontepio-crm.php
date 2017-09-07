e06ced2be6d923dec8ba5166cd667b16cc1d36c3
Project Manager <projectmanager@montepiodirect.com>
2017-09-07 12:43:10 +0200

    q

--- euromontepio-crm.php
+++ euromontepio-crm.php
@@ -203,8 +203,8 @@ function pp_wczc_page() {
 	echo('</div>'); // /wrap
 }
 
-//add_action('woocommerce_checkout_update_order_meta', 'pp_wczc_process_order');
-add_action('woocommerce_order_status_completed', 'convertir_lead_a_contacto');
+add_action('woocommerce_checkout_update_order_meta', 'convertir_lead_a_contacto');
+//add_action('woocommerce_order_status_completed', 'convertir_lead_a_contacto');
 add_action('user_register', 'enviar_usuario_a_zoho', 10, 1); 
 function enviar_usuario_a_zoho($user_id) {
     $usuario = get_userdata( $user_id );
